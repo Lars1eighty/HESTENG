fc0f29455bf790c3f46fc878157c7008bed09763
Lars1eighty <hesteng77@hotmail.com>
2026-09-26 00:21:44 +0200

    Forward scoped club night route params to scorer

--- hesteng-web/app/klubaften/[clubNightId]/kamp/[id]/page.tsx
+++ hesteng-web/app/klubaften/[clubNightId]/kamp/[id]/page.tsx
@@ -1,1 +1,7 @@
-export { default } from "../../../kamp/[id]/page";
+import ClubMatchPage from "../../../kamp/[id]/page";
+
+export default function ScopedClubMatchPage(props: {
+  params: Promise<{ clubNightId: string; id: string }>;
+}) {
+  return <ClubMatchPage {...props} />;
+}
