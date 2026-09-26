2d4aba6c2780d737c6cd6300ce2ddabeb97ba926
Lars1eighty <hesteng77@hotmail.com>
2026-09-26 09:49:50 +0200

    Scope nested club match route to its club night

--- hesteng-web/app/klubaften/[clubNightId]/kamp/[id]/page.tsx
+++ hesteng-web/app/klubaften/[clubNightId]/kamp/[id]/page.tsx
@@ -1,3 +1,18 @@
 "use client";
 
-export { default } from "../../../kamp/[id]/page";
+import { useEffect } from "react";
+import { useParams } from "next/navigation";
+import { useKlubaften } from "@/context/KlubaftenContext";
+import KampScoringPage from "../../../kamp/[id]/page";
+
+export default function ScopedKampScoringPage() {
+  const params = useParams<{ clubNightId?: string }>();
+  const { setCurrentClubNightId } = useKlubaften();
+  const clubNightId = typeof params.clubNightId === "string" ? params.clubNightId : null;
+
+  useEffect(() => {
+    if (clubNightId) setCurrentClubNightId(clubNightId);
+  }, [clubNightId, setCurrentClubNightId]);
+
+  return <KampScoringPage />;
+}
