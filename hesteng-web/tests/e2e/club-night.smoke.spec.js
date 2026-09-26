ad24ea4e6295e5ea4ac07091fb558107ece827fa
Lars1eighty <hesteng77@hotmail.com>
2026-09-26 10:23:55 +0200

    Verify Playwright console capture after scorer navigation

--- hesteng-web/tests/e2e/club-night.smoke.spec.js
+++ hesteng-web/tests/e2e/club-night.smoke.spec.js
@@ -130,6 +130,7 @@ test("real one-leg club match finishes at checkout and saves the result", async 
     localStorage.setItem("hesteng.sharedClubNightMigrated.v2", "true");
   }, { clubNights: [clubNight], currentClubNightId: "e2e-night" });
   await page.goto("http://127.0.0.1:3000/klubaften/e2e-night/kamp/e2e-match");
+  await page.evaluate(() => console.log("HESTENG_E2E_BROWSER_PROBE", { href: location.href, state: localStorage.getItem("hesteng.klubaftenState")?.length ?? 0 }));
   await page.waitForTimeout(1000);
   console.log("E2E URL:", page.url());
   console.log("E2E local state:", await page.evaluate(() => localStorage.getItem("hesteng.klubaftenState")));
